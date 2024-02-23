379b1a42e8102aff74a4564d22e9f037b045e92f
studiochampgauche <comptes@champgauche.studio>
2024-02-22 19:23:41 -0500

    Add Scroller

---
+++ src/components/Scroller.jsx
@@ -0,0 +1,40 @@
+import { useEffect } from 'react';
+//import { gsap } from 'gsap';
+//import { ScrollTrigger } from 'gsap/ScrollTrigger';
+//import { ScrollSmoother } from 'gsap/ScrollSmoother';
+
+const Scroller = ({ children }) => {
+	
+	
+	useEffect(() => {
+		
+        /*gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+        
+        
+        window.gscroll = ScrollSmoother.create({
+			wrapper: '#pageWrapper',
+			content: '#pageContent',
+			ignoreMobileResize: true,
+			smooth: 1
+		});*/
+		
+		return () => {
+			
+		}
+		
+		
+	}, []);
+	
+	
+	return(
+		 <>
+            <div id="pageWrapper">
+                <div id="pageContent">
+			        {children}
+                </div>
+            </div>
+		</>
+	)
+	
+}
+export default Scroller;
