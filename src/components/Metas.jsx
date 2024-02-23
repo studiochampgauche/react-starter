b2900ae33a4e3795c02f6c73ee7c26788956ba65
studiochampgauche <comptes@champgauche.studio>
2024-02-23 18:35:44 -0500

    Add Meta Robots management

--- src/components/Metas.jsx
+++ src/components/Metas.jsx
@@ -6,15 +6,57 @@ const Metas = () => {
     const { pathname } = useLocation();
     
     window.metadata = {
-        siteName: 'My App'
+        siteName: 'My App',
+        robots: {
+            index: true,
+            follow: true,
+            'max-image-preview': 'large'
+        }
     }
     
     useEffect(() => {
         
         /*
-        * Change Metas
+        * Title
         */
-        document.title = window.metadata.pageName + ' - ' + window.metadata.siteName;
+        const title = () => {
+            
+            document.title = pathname === '/' ? window.metadata.siteName : window.metadata.pageName + ' - ' + window.metadata.siteName;
+            
+        }
+        title();
+        
+        
+        /*
+        * Robots
+        */
+        const robots = () => {
+            
+            if(!window.metadata.robots || !Object.keys(window.metadata.robots).length) return;
+            
+            
+            let meta = document.querySelector('meta[name=robots]');
+            if(!meta){
+                
+                meta = document.createElement('meta');
+                meta.name = 'robots';
+                
+                document.head.prepend(meta);
+                
+            }
+            
+            meta.content = Object.keys(window.metadata.robots).map(key => {
+                
+                const value = window.metadata.robots[key];
+                
+                return value ? (value !== true ? `${key}:${value}` : key) : value;
+                
+            })
+            .filter(value => value)
+            .join(', ');
+            
+        }
+        robots();
         
 		
 		
