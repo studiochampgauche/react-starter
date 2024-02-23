7ed342c050abb5adfef310512fb38d90bbb8835f
studiochampgauche <comptes@champgauche.studio>
2024-02-22 20:59:52 -0500

    remove React from import

--- src/components/Transitor.jsx
+++ src/components/Transitor.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { useNavigate, useLocation } from 'react-router-dom';
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
